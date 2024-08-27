dafed99c1e6f6eb74c441e137d5b18934e00e894
Zaira Cruz <devzaira@gmail.com>
2024-08-28 05:09:23 +0800

    atm-user-interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BalanceComponent } from './balance/balance.component';
+import { DepositComponent } from './deposit/deposit.component';
+import { DepositCoinsComponent } from './deposit-coins/deposit-coins.component';
+import { DepositNotesComponent } from './deposit-notes/deposit-notes.component';
+import { WithdrawComponent } from './withdraw/withdraw.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceComponent,
+    DepositComponent,
+    DepositCoinsComponent,
+    DepositNotesComponent,
+    WithdrawComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
